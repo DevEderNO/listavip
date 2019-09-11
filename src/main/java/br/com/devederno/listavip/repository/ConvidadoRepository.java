947633827ac8ac97b9bb831e9d5f440b00d534c9
eder <helpmaster2@datarey.com.br>
2019-09-11 18:26:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/devederno/listavip/repository/ConvidadoRepository.java
@@ -0,0 +1,8 @@
+package br.com.devederno.listavip.repository;
+
+import br.com.devederno.listavip.model.Convidado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado,Long> {
+
+}
